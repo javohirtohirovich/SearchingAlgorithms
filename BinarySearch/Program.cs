e3a6d3c87ae8deb2bf27ec15b030e45afdcd2f4d
javohirtohirovich <javoergashev30@gmail.com>
2024-05-26 13:50:28 +0500

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BinarySearch;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine(BinarySearchFunction(arr, 9));
+    }
+
+    public static int BinarySearchFunction(int[] arr, int valSearch)
+    {
+        int startIndx = 0;
+        int endIndx = arr.Length - 1;
+        while (startIndx <= endIndx)
+        {
+            int mediumIndx = startIndx + (endIndx - startIndx) / 2;
+            if (arr[mediumIndx] == valSearch)
+            {
+                return mediumIndx;
+            }
+            else if (arr[mediumIndx] > valSearch)
+            {
+                endIndx = mediumIndx - 1;
+            }
+            else
+            {
+                startIndx = mediumIndx + 1;
+            }
+        }
+        return -1; 
+    }
+
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinearSearch;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine(LinearSearchFunction(arr, 23));
+    }
+
+    public static int LinearSearchFunction(int[] arr, int valSearch)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == valSearch)
+            {
+                return i; // Topilganda indeksni darhol qaytaramiz
+            }
+        }
+        return -1; // Agar topilmasa, -1 qaytariladi
+    }
+}
